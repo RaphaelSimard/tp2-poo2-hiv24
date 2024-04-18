1c46f0b939e2734fe0c444be26516453165b391c
TomGuyPatrick <2232285@cegeplimoilou.ca>
2024-04-17 22:03:38 -0400

    brouillon vrac ajouter produit
    brouillon vrac ajouter produit

--- tp2-etu/etu/magasin/src/main/java/tp2/etudiant/section/Vrac.java
+++ tp2-etu/etu/magasin/src/main/java/tp2/etudiant/section/Vrac.java
@@ -3,14 +3,13 @@ package tp2.etudiant.section;
 import tp2.application.AbstractProduit;
 import tp2.etudiant.boite.Boite;
 
-import java.util.Collection;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
-public class Vrac implements AireI{
+public class Vrac implements AireI {
 
 
     public final static int idDefault = 0;
+    double capaciteMax = 100;
     int sectionId;
     Map<String, Set<AbstractProduit>> contenu;
 
@@ -43,6 +42,26 @@ public class Vrac implements AireI{
     public boolean placerProduits(Collection<AbstractProduit> produits) {
         return false;
     }
+
+    public boolean ajouterProduit(HashSet<AbstractProduit> Produit) {
+        boolean ajout = false;
+        double volumeTotal = 0;
+        for (AbstractProduit produit : Produit) {
+//            volumeTotal += produit.getVolume;
+        }
+        if (volumeTotal <= capaciteMax) {
+            //Nom du bac au lieu de toString
+//            contenu.put(Produit.toString(), Produit);
+        } else {
+            while (volumeTotal > capaciteMax) {
+                for (AbstractProduit produits : Produit) {
+//                    contenu.put(Produit.toString(), produits);
+                }
+            }
+        }
+        return ajout;
+    }
+
 
     @Override
     public void viderAire() {
